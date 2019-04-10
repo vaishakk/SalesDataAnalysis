73d765de3edbcc5cb385fa4a29398a7e7826467f
Vaishak K <34245366+vaishakk@users.noreply.github.com>
2019-04-11 00:13:42 +0530

    Monthly and weekly sales accumulation

---
+++ acf_plot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pylab as pyl
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+from pandas.plotting import autocorrelation_plot
+
+def plotacf(data):
+	#Set figure parameters
+	rcParams['figure.figsize'] = 18, 9
+	rcParams['figure.dpi'] = 200
+	fig, ax1 = pyl.subplots()
+	color = 'tab:red'
+	ax1.set_xlabel('Date')
+	####
+	lns1 = ax1.plot(data.index.values, data.values, color=color,label='Sales')#Plot sales data
+	#Show and save plot
+	pyl.show()
+	#pyl.savefig('Sales.png')
+	#Plot acf
+	plt.figure()
+	autocorrelation_plot(data)
+	plt.show()
+	#plt.savefig('Correlation.png')
--- readData.py
+++ readData.py
@@ -3,25 +3,22 @@ import matplotlib.pylab as pyl
 import matplotlib.pyplot as plt
 from matplotlib.pylab import rcParams
 from pandas.plotting import autocorrelation_plot
+import acf_plot as ap
 
-salesData = pd.read_excel('total12345.xls',skiprows = [0], usecols = ['date','maxtemp','mdsetotal'])#Read data from file
+ndays = [31,28,31,30,31,30,31,31,30,31,30,31]
+dateParser = lambda x: pd.datetime.strptime(x,'%d-%m-%Y') if pd.notna(x) else nan
+salesData = pd.read_excel('total12345.xls',skiprows = [0], usecols = ['date','maxtemp','mdsetotal'], parse_dates=['date'],date_parser=dateParser)#Read data from file
 salesData.dropna(how = 'all',inplace = True)#Drop all empty rows
 #print(salesData.columns.values)
 salesData.columns = ['date','mdsetotal','maxtemp']#Change column names
+salesData.set_index('date',inplace = True)
 #print(salesData)
-#Set figure parameters
-rcParams['figure.figsize'] = 18, 9
-rcParams['figure.dpi'] = 200
-fig, ax1 = pyl.subplots()
-color = 'tab:red'
-ax1.set_xlabel('Date')
-####
-lns1 = ax1.plot(salesData.loc[:,'date'], salesData.loc[:,'mdsetotal'], color=color,label='Sales')#Plot sales data
-#Show and save plot
-#pyl.show()
-pyl.savefig('Sales.png')
-#Plot acf
-plt.figure()
-autocorrelation_plot(salesData['mdsetotal'])
-#plt.show()
-plt.savefig('Correlation.png')
+#print(salesData[pd.datetime.strptime('13-03-2016','%d-%m-%Y').strftime('%Y-%m-%d')])
+
+dailySales = pd.Series(salesData.loc[:,'mdsetotal'],index = salesData.index)
+weeklySales = dailySales.resample('W').mean()
+monthlySales = dailySales.resample('M').mean()
+#print(weeklySales.values)
+#print(monthlySales.index)
+
+ap.plotacf(weeklySales)
